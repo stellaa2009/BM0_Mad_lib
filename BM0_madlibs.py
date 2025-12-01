1e4abe1d5b20d72cee35877f29b7ceb7889b218a
stellaa2009 <stellaa19@nycstudents.net>
2025-12-01 10:30:44 -0500

    Update BM0_madlibs.py

--- BM0_madlibs.py
+++ BM0_madlibs.py
@@ -1,5 +1,5 @@
 import random
-story = random.randrange(1,2)
+story = random.randint(1,2)
 
 adj = input("Enter an adjective:")
 noun = input("Enter a noun:")
@@ -7,5 +7,5 @@ emo = input("Enter an emotion:")
 verb = input("Enter a verb (past tense):")
 if story == 1:
 	print(f"Today when I got on the train I saw a {adj} {noun}. I was so {emo} that I {verb}.")
-else :
-		print(f"I had a really {adj} day today. I got a 1 on the {noun} I made for art class. My teacher was so {emo} at me, that she {verb}")
+if story ==2 :
+	print(f"I had a really {adj} day today. I got a 1 on the {noun} I made for art class. My teacher was so {emo} at me, that she {verb}")
